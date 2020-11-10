37486d5bfb33c21160a06829e0a581e962aa6809
sergo1996 <56196384+sergo1996@users.noreply.github.com>
2020-11-10 23:50:19 +0200

    Updates

---
+++ static/js/photo-page.8acf23f0.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdentclinic=this.webpackJsonpdentclinic||[]).push([[5],{71:function(c,n,t){"use strict";t.r(n);var i=t(1);t(0);n.default=function(){return Object(i.jsx)("div",{children:Object(i.jsx)("h1",{children:"Photo"})})}}}]);
+//# sourceMappingURL=photo-page.8acf23f0.chunk.js.map
