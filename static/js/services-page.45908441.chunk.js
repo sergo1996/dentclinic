437569678b345c761978041e1ff3635d18ad0434
sergo1996 <56196384+sergo1996@users.noreply.github.com>
2020-12-10 19:31:57 +0200

    Updates

---
+++ static/js/services-page.45908441.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdentclinic=this.webpackJsonpdentclinic||[]).push([[13],{168:function(n,t,c){"use strict";c.r(t),c.d(t,"default",(function(){return r}));var e=c(52),i=c(2),s=(c(0),c(133));function r(n){return Object(i.jsx)("div",{children:Object(i.jsx)(s.a,Object(e.a)({},n))})}}}]);
+//# sourceMappingURL=services-page.45908441.chunk.js.map
